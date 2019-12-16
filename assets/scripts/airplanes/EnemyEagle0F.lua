1cb0dd9901f969e1b90b8745109ae37fd95f7bc6
Dragos <dragos.andreip@yahoo.com>
2019-12-17 00:04:25 +0200

    Airplanes can now have no weapons

--- assets/scripts/airplanes/EnemyEagle0F.lua
+++ assets/scripts/airplanes/EnemyEagle0F.lua
@@ -1,10 +1,7 @@
 local EnemyEagle0F = dofile('assets/scripts/airplanes/Eagle.lua')
 
 EnemyEagle0F.weapons = {
-    {
-        projectile = 'EnemyBullet0F',
-        ammo = 1
-    }
+    
 }
 
 return EnemyEagle0F
