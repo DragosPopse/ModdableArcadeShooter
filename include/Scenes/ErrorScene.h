43633b80dc9ff0fe098ee14406d80e364f2de0d1
Dragos <dragos.andreip@yahoo.com>
2019-12-20 13:08:36 +0200

    Added error scene

---
+++ include/Scenes/ErrorScene.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Scene.h"
+#include <TGUI/TGUI.hpp>
+#include <SFML/Graphics.hpp>
+#include <string>
+
+class ErrorScene :
+	public Scene
+{
+	tgui::Gui _gui;
+
+	tgui::Panel::Ptr _panel;
+	tgui::TextBox::Ptr _message;
+	tgui::Label::Ptr _title;
+	tgui::Button::Ptr _confirmButton;
+	std::shared_ptr<Scene> _nextScene;
+
+public:
+	ErrorScene(Context* context, const std::string& title, const std::string& message, std::shared_ptr<Scene> nextScene = nullptr);
+
+	bool HandleEvent(const sf::Event& ev);
+	bool Render();
+};
