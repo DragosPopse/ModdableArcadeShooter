e33447f1f6e2fc10cf26b3f4ba26eef1010c45d9
Dragos <dragos.andreip@yahoo.com>
2020-01-02 16:53:32 +0200

    Level1 balance

--- assets/scripts/airplanes/EnemyEagle0F.lua
+++ assets/scripts/airplanes/EnemyEagle0F.lua
@@ -1,5 +1,6 @@
 local EnemyEagle0F = dofile('assets/scripts/airplanes/Eagle.lua')
 
 EnemyEagle0F.weapons = nil
+EnemyEagle0F.speed = 300
 
 return EnemyEagle0F
--- assets/scripts/airplanes/EnemyEagleFBase.lua
+++ assets/scripts/airplanes/EnemyEagleFBase.lua
@@ -2,9 +2,11 @@ local EnemyEagleFBase = dofile('assets/scripts/airplanes/Eagle.lua')
 
 EnemyEagleFBase.weapons = {
 	{
-        projectile = 'EnemyBulletF',
+        projectile = 'EnemyBullet',
         ammo = -1
     }
 }
+
+EnemyEagleFBase.speed = 300
 
 return EnemyEagleFBase
